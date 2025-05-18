4c4f41eae69a7b1776d082cf4553cfaf002e53a8
strahinjazoranovic <6042877@mborijnland.nl>
2025-05-18 21:37:30 +0200

    V0.1.5.6

--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -3,13 +3,16 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="bg-white/10 z-1 fixed w-full backdrop-blur-lg border border-white/4 p-3">
+    <nav className="bg-white/10 z-1 fixed w-full backdrop-blur-lg border border-white/4 pl-0 pr-0 pt-3 pb-3">
       <div className="flex justify-around">
         <Link href="/">
-          <button className="btn btn bg-black">Home</button>
+          <button className="btn bg-black">Home</button>
         </Link>
         <Link href="/loads">
-          <button className="btn btn bg-black">Loadouts</button>
+          <button className="btn bg-black">Loadouts</button>
+        </Link>
+        <Link href="/create">
+          <button className="btn bg-black">Create Loadout</button>
         </Link>
       </div>
     </nav>
